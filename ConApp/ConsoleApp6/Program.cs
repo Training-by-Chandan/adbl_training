ba98684ff10b3062d82728d5799a1df818b00d93
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-06-30 20:12:41 +0545

    casting explained

--- ConApp/ConsoleApp6/Program.cs
+++ ConApp/ConsoleApp6/Program.cs
@@ -7,7 +7,71 @@ namespace ABC.App
         private static void Main()
         {
             //Basic();
-            DataTypes();
+            //DataTypes();
+            //Casting();
+            CastingExample();
+        }
+
+        private static void CastingExample()
+        {
+            Console.WriteLine("Enter the length");
+            string lengthStr = Console.ReadLine();
+            Console.WriteLine("Enter the breadth");
+            string breadthStr = Console.ReadLine();
+
+            int length = Convert.ToInt32(lengthStr);
+            int breadth = Convert.ToInt32(breadthStr);
+            int area = length * breadth;
+            int perimeter = 2 * (length + breadth);
+            Console.WriteLine("Area is " + area);
+            Console.WriteLine("Perimeter is " + perimeter);
+        }
+
+        //casting  : changing from one datatype to another datatype
+        private static void Casting()
+        {
+            //char => int => long => float => double
+            //-->---->----->----implicit casting---->------>------->
+            //<-----<-----<-----explicit casting<-----<-----<------<
+
+            //implicit : automatic casting
+            char c = 'C';
+            int i = c;// 67
+            long l = i;
+            float f = l;
+            double d = f;
+
+            //explicit : manual casting
+            f = (float)d;
+            l = (long)f;
+            i = (int)l;
+            c = (char)i;
+
+            //every types can be converted into string types or object types
+            string str = c.ToString();
+            str = i.ToString();
+            str = l.ToString();
+            str = f.ToString();
+
+            object o = str;
+            o = i;
+            o = c;
+            o = f;
+            o = l;
+            str = o.ToString();
+
+            //Parsing
+            str = "2.02";
+            f = float.Parse(str);
+            str = "1";
+            i = int.Parse(str);
+
+            //type conversion class
+            i = Convert.ToInt32(f);
+            i = Convert.ToInt32("123");
+
+            var j = "adfasd";
+            j = "";
         }
 
         private static void DataTypes()
@@ -33,7 +97,7 @@ namespace ABC.App
 
             bool b = true;
             char c = 'a';
-            string str = "djafhgsdfhajsdfasdfsdfgsdfgsdfgsdfgsdfgsdfgsdfgsdfgsdfgsdfgsdfgsdfgsdfgsdfgsdfgasdf";
+            string str = "10";
 
             DateTime dt = DateTime.Now;
         }
