a48b08b805e7a6f27414759c9b157171baaed6c9
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-08-14 20:12:46 +0545

    ado db connection completed

---
+++ Database/ConsoleApp.Db.Ado/ConsoleApp.Db.Ado/DbConnection.cs
@@ -0,0 +1,116 @@
+﻿using System.Data.SqlClient;
+
+namespace ConsoleApp.Db.Ado
+{
+    public static class DbConnection
+    {
+        private static readonly string _connectionString = "Data Source=DESKTOP-PT71T7O\\SQLBHAGAT;Initial Catalog=Adb.Console.Ado;integrated security=true;";
+
+        public static void ReadAll()
+        {
+            ///1. Create the query
+            string query = "select * from Person";
+
+            ReadData(query);
+        }
+
+        public static void ReadById()
+        {
+            Console.WriteLine("Enter the Id");
+            var id = Console.ReadLine();
+            ///1. Create the query
+            string query = $"select * from Person where id = {id}";
+
+            ReadData(query);
+        }
+
+        public static void Create()
+        {
+            Console.WriteLine("Enter the First Name");
+            var fname = Console.ReadLine();
+            Console.WriteLine("Enter the Last Name");
+            var lname = Console.ReadLine();
+            Console.WriteLine("Enter the Email");
+            var email = Console.ReadLine();
+            ///1. Create the query
+            string query = $"Insert into Person (FirstName, LastName,Email) values  ('{fname}','{lname}','{email}')";
+
+            ExecuteNonQuery(query);
+        }
+
+        public static void Edit()
+        {
+            Console.WriteLine("Enter the Id");
+            var id = Console.ReadLine();
+            Console.WriteLine("Enter the First Name");
+            var fname = Console.ReadLine();
+            Console.WriteLine("Enter the Last Name");
+            var lname = Console.ReadLine();
+            Console.WriteLine("Enter the Email");
+            var email = Console.ReadLine();
+            ///1. Create the query
+            string query = $"update Person set FirstName = '{fname}', LastName ='{lname}', Email = '{email}' where id={id}";
+
+            ExecuteNonQuery(query);
+        }
+        public static void Delete()
+        {
+            Console.WriteLine("Enter the Id");
+            var id = Console.ReadLine();
+          
+            ///1. Create the query
+            string query = $"delete from Person where id={id}";
+
+            ExecuteNonQuery(query);
+        }
+
+
+        private static void ReadData(string query)
+        {
+            ///2. Create the Sql Connection Object using the connectionstring
+            SqlConnection con = new SqlConnection(_connectionString);
+
+            ///3. Using the connection object and query, we will create the sql command object
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            ///4. Open the connection
+            con.Open();
+
+            ///5. Run the command
+            var reader = cmd.ExecuteReader();
+
+            ///6. Do the processing
+            Console.WriteLine("Data From the table Person");
+            Console.WriteLine("======================================");
+            while (reader.Read())
+            {
+                Console.WriteLine($"Id : {reader.GetFieldValue<int>(0)}");
+                Console.WriteLine($"First Name : {reader.GetFieldValue<string>(1)}");
+                Console.WriteLine($"Last Name : {reader.GetFieldValue<string>(2)}");
+                Console.WriteLine($"Email : {reader.GetFieldValue<string>(3)}");
+                Console.WriteLine("======================================");
+            }
+
+            ///7. Close the connection
+            con.Close();
+        }
+
+        private static void ExecuteNonQuery(string query)
+        {
+            ///2. Create the Sql Connection Object using the connectionstring
+            SqlConnection con = new SqlConnection(_connectionString);
+
+            ///3. Using the connection object and query, we will create the sql command object
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            ///4. Open the connection
+            con.Open();
+
+            ///5. Run the command
+             cmd.ExecuteNonQuery();
+
+            ///7. Close the connection
+            con.Close();
+        }
+    }
+}
---
+++ Database/ConsoleApp.Db.Ado/ConsoleApp.Db.Ado/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ConsoleApp.Db.Ado
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var res = "N";
+            do
+            {
+                DbOperation();
+
+                Console.WriteLine("do you want to continute more(Y/N)?");
+                res = Console.ReadLine();
+            } while (res.ToUpper() == "Y");
+        }
+
+        static void DbOperation()
+        {
+            Console.WriteLine("Press\n1 to Read all the data\n2 to Read data by ID\n3 to Create the Record\n4 to Update the Record\n5 to Delete the Record");
+            var choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    DbConnection.ReadAll();
+                    break;
+                case 2:
+                    DbConnection.ReadById();
+                    break;
+                case 3:
+                    DbConnection.Create();
+                    break;
+                case 4:
+                    DbConnection.Edit();
+                    break;
+                case 5:
+                    DbConnection.Delete();
+                    break;
+                default:
+                    Console.WriteLine("Invalid Choice");
+                    break;
+            }
+        }
+    }
+}
